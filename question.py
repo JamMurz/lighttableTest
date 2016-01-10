0f2a0b6774c3b15cad63299d7cd0729ca6b08dc9
John Murzaku <jam@lissus.com>
2016-01-09 19:30:05 -0500

    Added python program

---
+++ question.py
@@ -0,0 +1,5 @@
+age = input("Please enter your age: ")
+if age < 21:
+  print("Sorry, you are not of legal age")
+elif age >= 21:
+  print("Welcome! You are of legal age what do you want to drink?")
